479e08758b2c1994d31e281c602499eb658d2d6f
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-05-19 22:12:05 +0100

    adding WIP component to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,41 +3,19 @@ import { Link } from "gatsby"
 import { PageLayout } from "../components/layout"
 import { Card, CardContent, CardContainer, CardTitle, CardMedia, CardActions } from "../components/card"
 import { ImgGatsbyAstronaut } from "../components/images"
+import { StoolTypeCapture } from "../components/form/stool"
+
+const WIP = () => {
+  return (
+    <StoolTypeCapture />
+  )
+}
 
 const IndexPage = () => (
   <PageLayout title="Home">
     <div>
       <h1>Hi people</h1>
-      <CardContainer>
-        <Card>
-          <CardTitle>Card 1</CardTitle>
-          <CardContent>
-            This is Card One content with just actions
-          </CardContent>
-          <CardActions>
-            <button>Button 1</button>
-            <button>Button 2</button>
-            <button>Button 3</button>
-          </CardActions>
-        </Card>
-        <Card>
-          <CardMedia gatsbyImageComp={<ImgGatsbyAstronaut />} />
-          <CardTitle>Card 2</CardTitle>
-          <CardContent>
-            This is Card Two content with just media
-          </CardContent>
-        </Card>
-        <Card>
-          <CardMedia gatsbyImageComp={<ImgGatsbyAstronaut />} />
-          <CardTitle>Card 3</CardTitle>
-          <CardContent>
-            This is Card Three content that is much bigger than the other cards and has both actions and media.
-          </CardContent>
-          <CardActions>
-            <button>Button 1</button>
-          </CardActions>
-        </Card>
-      </CardContainer>
+      <WIP />
     </div>
     <ul>
       <li>
--- src/pages/page-1.js
+++ src/pages/page-1.js
@@ -1,25 +1,54 @@
 import React from "react"
 import { Link } from "gatsby"
 import { PageLayout } from "../components/layout"
+import { Card, CardContent, CardContainer, CardTitle, CardMedia, CardActions } from "../components/card"
 import { ImgGatsbyAstronaut } from "../components/images"
 
-const FirstPage = () => (
-  <PageLayout title="Page one">
-    <h1>Hi from the first page</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <ImgGatsbyAstronaut />
+const IndexPage = () => (
+  <PageLayout title="Page One">
+    <div>
+      <h1>Hi people</h1>
+      <CardContainer>
+        <Card>
+          <CardTitle>Card 1</CardTitle>
+          <CardContent>
+            This is Card One content with just actions
+          </CardContent>
+          <CardActions>
+            <button>Button 1</button>
+            <button>Button 2</button>
+            <button>Button 3</button>
+          </CardActions>
+        </Card>
+        <Card>
+          <CardMedia gatsbyImageComp={<ImgGatsbyAstronaut />} />
+          <CardTitle>Card 2</CardTitle>
+          <CardContent>
+            This is Card Two content with just media
+          </CardContent>
+        </Card>
+        <Card>
+          <CardMedia gatsbyImageComp={<ImgGatsbyAstronaut />} />
+          <CardTitle>Card 3</CardTitle>
+          <CardContent>
+            This is Card Three content that is much bigger than the other cards and has both actions and media.
+          </CardContent>
+          <CardActions>
+            <button>Button 1</button>
+          </CardActions>
+        </Card>
+      </CardContainer>
     </div>
     <ul>
       <li>
-        <Link to="/">Go home</Link>
+        <Link to="/page-1/">Go to page 1</Link>
       </li>
       <li>
         <Link to="/page-2/">Go to page 2</Link>
       </li>
     </ul>
   </PageLayout>
+
 )
 
-export default FirstPage
+export default IndexPage
