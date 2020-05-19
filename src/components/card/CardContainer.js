f3d909becef9a49b2d0c68f11161a480ba45797f
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-05-19 22:11:17 +0100

    adding columns to CardContainer

--- src/components/card/CardContainer.js
+++ src/components/card/CardContainer.js
@@ -5,16 +5,36 @@ import PropTypes from 'prop-types'
 const CardContainerStyle = styled.div`
   margin: auto;
   display: flex;
-  flex-direction: ${props => props.column ? 'column' : 'row'};
+  flex-direction: ${props => props.colDirection};
+  flex-wrap: wrap;
   word-wrap: break-word;
   outline: 0;
   position: relative;
   justify-content: center;
+  
+  // TODO need to consider CSS Grid here instead
+  > div {
+    ${props => {
+    let flexCol = `flex: 1 1 `;
+    switch (props.colNum) {
+      case 1: flexCol += '440px;'; break;
+      case 2: flexCol += '290px;'; break;
+      case 3: flexCol += '215px;'; break;
+      case 4: flexCol += '170px;'; break;
+      case 5: flexCol += '140px;'; break;
+      default: flexCol = ``;
+    }
+    return flexCol;
+  }}
+  }
 `
 
-const CardContainer = ({ column = false, children }) => {
+const CardContainer = ({ colNum = 0, colDirection = 'row', children }) => {
+
+
+
   return (
-    <CardContainerStyle column={column}>
+    <CardContainerStyle colDirection={colDirection} colNum={colNum} >
       {children}
     </CardContainerStyle>
   )
