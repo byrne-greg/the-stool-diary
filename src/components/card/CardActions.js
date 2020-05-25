80cdee450ffc9b49d3992fb0e1010f3d13be4ee6
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-05-25 22:51:46 +0100

    adding responsive css around cars action buttons

--- src/components/card/CardActions.js
+++ src/components/card/CardActions.js
@@ -5,16 +5,37 @@ import PropTypes from 'prop-types'
 const CardActionsStyle = styled.div`
   padding: 0.5rem 0;
   margin-top: auto;
-  
-  // every button descendent thats not first child
-  button :not(:first-child) {  
-    margin-left: 1rem;
+
+  // if we are on small device, make buttons blocky
+  @media (max-width: 800px) {
+    display: flex;
+    flex-direction: column;
+    button { 
+      width: 100%; 
+      margin: 0.5rem 0;
+    }
   }
-`
+
+  // if we are on large device, make buttons normal size with margin spaces
+  @media (min-width: 800px) {
+    // every button descendent thats not first child
+    button :not(:first-child) {  
+      margin-left: 1rem;
+    }
+  }
+  
+  // if we have only one child element (presume button), make the button blocky
+  ${({ actionCount }) => actionCount === 1 && `
+    button { 
+      width: 100%; 
+    }
+  `}
+     
+  `
 
 const CardActions = ({ children }) => {
   return (
-    <CardActionsStyle>
+    <CardActionsStyle actionCount={React.Children.count(children)}>
       {children}
     </CardActionsStyle>
   )
