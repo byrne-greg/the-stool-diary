a6771695c7d40ba1eaee241ca1c0cc1e40575d65
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-05-28 20:44:13 +0100

    adding Card and storybook addon

--- src/components/card/__stories__/Card.stories.js
+++ src/components/card/__stories__/Card.stories.js
@@ -1,15 +1,61 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Card, SpacedCard, CardContent, CardTitle } from '../'
+import CardActions from '../CardActions';
 
 
 export default {
   title: 'Card',
 };
 
-export const Card_with_text = () => <Card onClick={action('card clicked')}>Card with Text</Card>
-export const Card_with_CardContent = () => <SpacedCard onClick={action('card clicked')}><CardContent>Card with CardContent</CardContent></SpacedCard>
-export const Card_with_CardTitle = () => <SpacedCard onClick={action('card clicked')}><CardTitle>Card with CardTitle</CardTitle></SpacedCard>
+export const Card_with_text = () => (<>
+  <Card onClick={action('card clicked')}>Card with Text</Card>
+</>)
+export const Card_with_CardContent = () => (<>
+  <Card onClick={action('card clicked')}>
+    <CardContent>Card with CardContent</CardContent>
+  </Card>
+</>)
+export const Card_with_CardTitle = () => (<>
+  <Card onClick={action('card clicked')}>
+    <CardTitle>Card with CardTitle</CardTitle>
+  </Card>
+</>)
+export const Card_with_CardTitle_and_CardContent = () => (<>
+  <Card onClick={action('card clicked')}>
+    <CardTitle>Card Title</CardTitle>
+    <CardContent>This is the CardContent</CardContent>
+  </Card>
+</>);
 
-export const Spaced_Card_with_text = () => <SpacedCard onClick={action('card clicked')}>Card with Outer Margin</SpacedCard>
+export const Card_with_CardActions_OneItem = () => (<>
+  <Card onClick={action('card clicked')}>
+    <CardTitle>Card Title</CardTitle>
+    <CardContent>This is the CardContent</CardContent>
+    <CardActions><button>a button</button></CardActions>
+  </Card>
+</>);
+
+export const Card_with_CardActions_MultipleItems = () => (<>
+  <Card onClick={action('card clicked')}>
+    <CardTitle>Card Title</CardTitle>
+    <CardContent>This is the CardContent</CardContent>
+    <CardActions>
+      <button>a button</button>
+      <button>a button</button>
+      <button>a button</button>
+    </CardActions>
+  </Card>
+</>);
+
+export const Spaced_Card_with_text = () => (<>
+  <SpacedCard onClick={action('card clicked')}>
+    <CardTitle>Card 1</CardTitle>
+    <CardContent>Cards with Outer Margin</CardContent>
+  </SpacedCard>
+  <SpacedCard onClick={action('card clicked')}>
+    <CardTitle>Card 2</CardTitle>
+    <CardContent>Cards with Outer Margin</CardContent>
+  </SpacedCard>
+</>)
 
