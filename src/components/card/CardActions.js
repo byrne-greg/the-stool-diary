774f2d85f4a9d10a3f9a4d529ed48d2b5f5ed418
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-05-28 20:44:22 +0100

    fixing Card css

--- src/components/card/CardActions.js
+++ src/components/card/CardActions.js
@@ -11,32 +11,31 @@ const CardActionsStyle = styled.div`
     padding: 0.5rem 0 0 0;
     display: flex;
     flex-direction: column;
-    button { 
+    * { 
       width: 100%; 
-    }
-
-    button :last-child {
-      border-radius: 0 0 16px 16px;
+      :last-child {
+        border-radius: 0 0 16px 16px;
+      }
     }
   }
 
   // if we are on large device, make buttons normal size with margin spaces
   @media (min-width: 800px) {
-    padding: 1rem 1rem;
-    // every button descendent thats not first child
-    button :not(:first-child) {  
-      margin-left: 1rem;
-      border-radius: 0 0 16px 16px;
-    }
+    
+    // if we have only one child element (presume button), make the button blocky
+    ${({ actionCount }) => actionCount === 1 ? `
+      padding: 0.5rem 0 0 0;
+      * { 
+        width: 100%; 
+        border-radius: 0 0 16px 16px;
+      }
+    ` : `
+      padding: 1rem 1rem;
+      // every descendent thats not first child
+      *:not(:first-child) {  
+          margin-left: 1rem;
+      }`}
   }
-  
-  // if we have only one child element (presume button), make the button blocky
-  ${({ actionCount }) => actionCount === 1 && `
-    button { 
-      width: 100%; 
-    }
-  `}
-     
   `
 
 const CardActions = ({ children }) => {
