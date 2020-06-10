a8a6188e1bd702c23bcbcde331ded3945ceb7ba2
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-06-10 21:37:51 +0100

    removing field attribute for db

--- src/components/firebase/utils.js
+++ src/components/firebase/utils.js
@@ -1,9 +1,7 @@
 import firebase from 'gatsby-plugin-firebase'
 
-const db = firebase.firestore();
-
 export const persistData = (namespace, obj) => {
-  db.collection(namespace).add({ ...obj })
+  firebase.firestore().collection(namespace).add({ ...obj })
     // .then(function (docRef) {
     //   console.log("Document written with ID: ", docRef.id);
     // })
@@ -16,7 +14,7 @@ export const retrieveData = (namespace) => {
 
   async function getData(namespace) {
     const data = [];
-    await db.collection(namespace).get().then((querySnapshot) => {
+    await firebase.firestore().collection(namespace).get().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
         console.log(`${doc.id} => ${doc.data()}`);
         data.push({ ...doc.data(), id: doc.id })
