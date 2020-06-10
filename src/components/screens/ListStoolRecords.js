004b5dff4a5b4d87f406ce320228cfeabe0846e0
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-06-10 14:21:04 +0100

    adding some base styling to list stool records screen

--- src/components/screens/ListStoolRecords.js
+++ src/components/screens/ListStoolRecords.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import moment from 'moment'
+import stoolClassifications from '../../utils/stool-classifications'
 
 
 const SAMPLE_TEST_DATA = [
@@ -18,6 +19,31 @@ const SAMPLE_TEST_DATA = [
   }
 ]
 
+
+const ListItem = styled.li`
+  margin: 0;
+  padding: 1rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const ListItemAvatar = styled.div`
+  padding: 0 0.5rem;
+  width: 10rem;
+`
+const ListItemTitle = styled.div`
+  padding: 0 0.5rem;
+`
+const ListItemDescription = styled.div`
+  padding: 0 0.5rem;
+`
+
+const ListItemTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
 const ListStoolRecords = () => {
 
   const [stoolRecords, setStoolRecords] = useState([]);
@@ -29,9 +55,19 @@ const ListStoolRecords = () => {
   return (
     <>
       <ul>
-        {stoolRecords.map((stoolRecord) => <>
-          <li>Type: {stoolRecord.type} - Date/Time: {moment(stoolRecord.datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")}</li>
-        </>)}
+        {stoolRecords.map((stoolRecord) => {
+          const stoolClass = stoolClassifications.find(stoolClass => stoolClass.type === stoolRecord.type)
+          console.log(stoolClass);
+          return (<>
+            <ListItem key={`${stoolClass.type}-${stoolClass.datetime}`}>
+              <ListItemAvatar>{stoolClass.image}</ListItemAvatar>
+              <ListItemTextContainer>
+                <ListItemTitle>Type: {stoolRecord.type}</ListItemTitle>
+                <ListItemDescription>Date/Time: {moment(stoolRecord.datetime).format("dddd, MMMM Do YYYY, h:mm:ss a")}</ListItemDescription>
+              </ListItemTextContainer>
+            </ListItem>
+          </>)
+        })}
       </ul>
     </>
   )
