4651409069eafd89c3de303ca32269fbad17b072
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-06-10 13:35:52 +0100

    removing wip placeholder record stool form screens

--- src/pages/record-stool.js
+++ src/pages/record-stool.js
@@ -2,17 +2,15 @@
 import React from "react"
 import { Link } from "gatsby"
 import { PageLayout } from "../components/layout"
-import { StoolTypeCapture } from "../components/form/stool"
+import RecordStoolForm from "../components/screens/RecordStoolForm"
+
 
 
 const RecordStoolPage = () => (
   <PageLayout title="Record Stool Type">
-    <h1>Record Stool Type Form Page</h1>
-    <StoolTypeCapture />
+    <RecordStoolForm />
+    <div></div>
     <ul>
-      <li>
-        <Link to="/record-stool-date-time/">Go to Record Stool Date Time</Link>
-      </li>
       <li>
         <Link to="/">Go home</Link>
       </li>
