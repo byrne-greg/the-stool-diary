d08957e11d7aa4261f1bdc139bac184402acbbfa
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-06-12 16:05:24 +0100

    add go home link from 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,6 +6,7 @@ const NotFoundPage = () => (
   <PageLayout title="404: Not found" >
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Link to="/">Go home</Link>
   </PageLayout>
 )
 
