711c4d418b6ed6cbe0af80c9c81db05e66320876
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-06-15 17:12:47 +0100

    fix test with temp workaround

--- src/components/form/stool/__tests__/StoolSizeCapture.test.js
+++ src/components/form/stool/__tests__/StoolSizeCapture.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react'
 import StoolSizeCapture from '../StoolSizeCapture';
+import defaultLocale from '../locales/StoolSizeCapture.locale.en.json'
 
 describe('StoolSizeCapture', () => {
   describe('Persistence', () => {
     test('when small stool size is selected, then given size is persisted', async () => {
 
       // ARRANGE
+      const localeTextKey = defaultLocale.small.toLowerCase();
       let valueToPersist = null
       const persistSize = (value) => valueToPersist = value;
 
@@ -14,10 +16,10 @@ describe('StoolSizeCapture', () => {
       const { getByText, findByText } = render(
         <StoolSizeCapture persistSize={persistSize} />
       )
-      fireEvent.click(getByText('Small'))
+      fireEvent.click(getByText(localeTextKey))
 
       // ASSERT
-      expect(valueToPersist).toBe('Small')
+      expect(valueToPersist).toBe(defaultLocale.small)
     });
   });
 });
