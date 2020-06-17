25ba112f46f48d52e503e98380b91d08ebe9bdb3
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-06-17 22:29:48 +0100

    add stool datetime format pattern to the stool model

--- src/components/form/stool/state/stoolModel.js
+++ src/components/form/stool/state/stoolModel.js
@@ -2,4 +2,6 @@ export const INITIAL_STOOL_STATE = {
   type: null,
   dateTime: { timestamp: null, dateString: null, dateOnly: true },
   size: null,
-} 
+}
+
+export const STOOL_DATESTRING_FORMAT = "YYYY-MM-DD"
