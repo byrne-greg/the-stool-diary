459088e5aeb706efeb14e8a6bb5fdaf9b90ce65f
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-06-19 11:55:55 +0100

    add sadface emoji to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import { PageLayout } from "../components/layout"
 const NotFoundPage = () => (
   <PageLayout title="404: Not found" >
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that doesn&#39;t exist... the sadness 😞</p>
     <Link to="/">Go home</Link>
   </PageLayout>
 )
