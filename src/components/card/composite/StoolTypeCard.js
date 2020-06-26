51784b6027359f89ec6841f139a3c0a7c08d5480
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-06-26 13:46:46 +0100

    add datatestid override to StoolTypeCard

--- src/components/card/composite/StoolTypeCard.js
+++ src/components/card/composite/StoolTypeCard.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Card, CardMedia, CardTitle, CardContent, CardActions } from '..'
 import { PrimaryActionButton, SecondaryActionButton } from '../../button'
 
-const StoolTypeCard = ({ type, image, description, handleClick, isSelected }) => {
+const StoolTypeCard = ({ type, image, description, handleClick, isSelected, ...props }) => {
 
   const selectCardFn = () => handleClick(type)
   const unselectCardFn = () => handleClick(null);
 
   return (
     <>
-      <Card data-testid={`stool-type-card-type-${type}`} onClick={!isSelected ? selectCardFn : unselectCardFn} >
+      <Card data-testid={`stool-type-card-type-${type}`} {...props} onClick={!isSelected ? selectCardFn : unselectCardFn} >
         <CardMedia imgComp={image} />
         <CardTitle>Type {type}</CardTitle>
         <CardContent>
