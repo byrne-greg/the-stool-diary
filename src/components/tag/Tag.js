639b455f1f9f0dfbcb264d905b1cb92ee0da8fa6
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-06-29 11:18:10 +0100

    fix tag default color

--- src/components/tag/Tag.js
+++ src/components/tag/Tag.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import colors from '../../utils/colors'
 
 export const FilledTagStyle = styled.span`
   padding: 0.3rem 0.5rem;
   margin: 0;
   border-radius: 8px 8px 8px 8px;
 
-  background-color: ${({ tagColor = color.PRIMARY }) => tagColor};
-  border: 2px solid ${({ tagColor = color.PRIMARY }) => tagColor};
+  background-color: ${({ tagColor = colors.BLUE }) => tagColor};
+  border: 2px solid ${ ({ tagColor = colors.BLUE }) => tagColor};
   color: white;
 `
 const Tag = ({ children, ...props }) => (
-  <FilledTagStyle  {...props} >{children}</FilledTagStyle>)
+  <FilledTagStyle {...props} >{children}</FilledTagStyle>)
 
 
 export default Tag;
