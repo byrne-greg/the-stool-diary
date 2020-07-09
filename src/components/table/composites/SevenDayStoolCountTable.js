cfdf2b1bcbc04a27a69d9109be1290ae3e1e6456
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-07-09 16:07:36 +0100

    refactor SevenDayStoolCountTable and add translation

--- src/components/table/composites/SevenDayStoolCountTable.js
+++ src/components/table/composites/SevenDayStoolCountTable.js
@@ -1,62 +1,68 @@
-import React, { useEffect } from 'react'
+import React, { useMemo } from 'react'
 import moment from 'moment'
 import styled from 'styled-components'
+import { useTranslation } from 'react-i18next'
 
 
+const TableHeaderCell = styled.th`
+  text-align: ${({ center = false }) => center ? 'center' : 'left'}
+`
+const TableBodyCell = styled.td`
+  text-align: ${({ center = false }) => center ? 'center' : 'left'}
+`
 
 const SevenDayStoolCountTable = ({ recordedStools = [] }) => {
 
-  // get the timestamps for the last seven days (incl today)
-  // sort recordedStools by latest timestamp
-  // loop through recordedStools
-  //    if recordedStool date matches a date in the date range, add one to it's count
-  //    if timestamp is outside the last seven days, break loop
-  // loop through last seven days data
-  //    display
+  const { t } = useTranslation();
 
+  const lastSevenDaysDataForDisplay = useMemo(() => {
+    const formatToDateString = (moment) => moment.format('YYYY-MM-DD')
+    const currentDateString = formatToDateString(moment());
+    const startDateString = formatToDateString(moment().subtract(7, 'days'))
 
-  const lastSevenDaysData = [{ dateString: moment().format('YYYY-MM-DD'), count: 0 }];
-  for (var i = 1; i < 7; i++) {
-    lastSevenDaysData.unshift({ dateString: moment().subtract(i, 'days').format('YYYY-MM-DD'), count: 0 })
-  }
-
-  recordedStools.forEach(recordedStool => {
-    console.log('collecting 7daydata', recordedStool)
-    console.log('collecting 7daydata - is moment inbetween', moment(recordedStool.dateTime.dateString).isBetween(moment().subtract(7, 'day').format("YYYY-MM-DD"), moment().format("YYYY-MM-DD"), 'day'))
-    console.log('collecting 7daydata - recodedStoolDay', moment(recordedStool.dateTime.dateString).format('YYYY-MM-DD'))
-    console.log('collecting 7daydata - startRange', moment().subtract(7, 'days').format('YYYY-MM-DD'))
-    console.log('collecting 7daydata - endRange', moment().format('YYYY-MM-DD'))
-    if (moment(recordedStool.dateTime.dateString).isSameOrAfter(moment().subtract(7, 'days'), 'day')
-      && moment(recordedStool.dateTime.dateString).isSameOrBefore(moment(), 'day')
-    ) {
-      lastSevenDaysData.forEach(dayData => {
-        console.log('collecting 7daydata - does recorded stool datestring match 7day datestring', `${recordedStool.dateTime.dateString} === ${dayData.dateString}`, recordedStool.dateTime.dateString === dayData.dateString)
-        if (recordedStool.dateTime.dateString === dayData.dateString) {
-          dayData.count++
-        }
-      })
+    // Build the data model for the last seven days
+    // Do this separately from the recordedStools as they may have no records for the last seven days
+    // Initialize the seven day data with a default count of zero
+    const lastSevenDaysData = [{ dateString: currentDateString, count: 0 }];
+    for (var i = 1; i < 7; i++) {
+      lastSevenDaysData.unshift({ dateString: formatToDateString(moment().subtract(i, 'days')), count: 0 })
     }
-  })
 
-  console.log('lastSevenDaysData', lastSevenDaysData)
+    // Create a copy of the recorded stool data sorted by date so we have the most recent records first
+    const sortedRecordedStools = recordedStools.slice().sort((a, b) => new Date(b.dateTime.timestamp) - new Date(a.dateTime.timestamp))
+
+    // Now loop through the recorded stool data
+    // Check each stool datetime date to see if it's within in our seven day range
+    // If we find one, increment the seven day data item's count
+    sortedRecordedStools.filter(recordedStool =>
+      moment(recordedStool.dateTime.dateString).isSameOrAfter(startDateString, 'day')
+      && moment(recordedStool.dateTime.dateString).isSameOrBefore(currentDateString, 'day')).forEach(recordedStoolInDateRange => {
+        const dayDataItem = lastSevenDaysData.find(dayData => dayData.dateString === recordedStoolInDateRange.dateTime.dateString)
+        dayDataItem.count++;
+      })
+
+    return lastSevenDaysData;
+  }, [recordedStools])
+
+
 
   return (
     <>
-      <h3>Last seven days stool count</h3>
+      <h3>{t('Last seven days stool count')}</h3>
       <table>
         <thead>
           <tr>
-            <th>Day</th>
-            <th>Stool Count</th>
+            <TableHeaderCell>{t('Day')}</TableHeaderCell>
+            <TableHeaderCell center>{t('Stool Count')}</TableHeaderCell>
           </tr>
         </thead>
         <tbody>
-          {lastSevenDaysData.map(datetime =>
-            <tr key={`${datetime.dateString}-${datetime.count}`}>
-              <td>{moment(datetime.dateString).format('dddd, Do MMM')}</td>
-              <td >
+          {lastSevenDaysDataForDisplay.map(datetime =>
+            <tr key={`${datetime.dateString} -${datetime.count} `}>
+              <TableBodyCell>{moment(datetime.dateString).format('dddd, Do MMM')}</TableBodyCell>
+              <TableBodyCell center>
                 <span style={{ backgroundColor: 'yellow', textAlign: 'center', padding: 10 }}>{datetime.count}</span>
-              </td>
+              </TableBodyCell>
             </tr>
           )}
         </tbody>
