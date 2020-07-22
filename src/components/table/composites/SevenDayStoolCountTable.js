0240f3639c6d803daa3864f12fea6d7d0374f3c4
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-07-22 10:53:50 +0100

    adding SevenDayStoolCount table component

--- src/components/table/composites/SevenDayStoolCountTable.js
+++ src/components/table/composites/SevenDayStoolCountTable.js
@@ -1,7 +1,11 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import moment from 'moment'
 import styled from 'styled-components'
+import { IconButton } from '@material-ui/core';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import { useTranslation } from 'react-i18next'
+import { ListStoolRecords } from '../../list/composites'
 
 
 const TableHeaderCell = styled.th`
@@ -24,7 +28,6 @@ const StoolCount = styled.span`
       case 3: color = `cornflowerblue`; break;
 
     }
-    console.log(count, color)
     return color;
   }}
 `
@@ -38,12 +41,12 @@ const SevenDayStoolCountTable = ({ recordedStools = [] }) => {
     const currentDateString = formatToDateString(moment());
     const startDateString = formatToDateString(moment().subtract(7, 'days'))
 
-    // Build the data model for the last seven days
+    // Build the data construct for the last seven days
     // Do this separately from the recordedStools as they may have no records for the last seven days
     // Initialize the seven day data with a default count of zero
-    const lastSevenDaysData = [{ dateString: currentDateString, count: 0 }];
-    for (var i = 1; i < 7; i++) {
-      lastSevenDaysData.unshift({ dateString: formatToDateString(moment().subtract(i, 'days')), count: 0 })
+    const lastSevenDaysData = [{ dateString: currentDateString, count: 0, stools: [] }];
+    for (var i = 1; i <= 7; i++) {
+      lastSevenDaysData.unshift({ dateString: formatToDateString(moment().subtract(i, 'days')), count: 0, stools: [] })
     }
 
     // Create a copy of the recorded stool data sorted by date so we have the most recent records first
@@ -57,37 +60,70 @@ const SevenDayStoolCountTable = ({ recordedStools = [] }) => {
       && moment(recordedStool.dateTime.dateString).isSameOrBefore(currentDateString, 'day')).forEach(recordedStoolInDateRange => {
         const dayDataItem = lastSevenDaysData.find(dayData => dayData.dateString === recordedStoolInDateRange.dateTime.dateString)
         dayDataItem.count++;
+        dayDataItem.stools.push(recordedStoolInDateRange);
       })
 
     return lastSevenDaysData;
   }, [recordedStools])
 
 
-
+  const isCollapsible = true;
   return (
     <>
       <h3>{t('Last seven days stool count')}</h3>
       <table>
         <thead>
           <tr>
+            {isCollapsible && <TableHeaderCell />}
             <TableHeaderCell>{t('Day')}</TableHeaderCell>
             <TableHeaderCell center>{t('Stool Count')}</TableHeaderCell>
           </tr>
         </thead>
         <tbody>
-          {lastSevenDaysDataForDisplay.map(datetime =>
-            <tr key={`${datetime.dateString} -${datetime.count} `}>
-              <TableBodyCell>{moment(datetime.dateString).format('dddd, Do MMM')}</TableBodyCell>
-              <TableBodyCell center>
-                <StoolCount count={datetime.count}>{datetime.count}</StoolCount>
-                {/* <span style={{ backgroundColor: 'yellow', textAlign: 'center', padding: 10 }}>{datetime.count}</span> */}
-              </TableBodyCell>
-            </tr>
+          {lastSevenDaysDataForDisplay.map(dayDataItem =>
+            <CollapsibleRow rowInfo={dayDataItem} key={`${dayDataItem.dateString}-${dayDataItem.count}`} />
           )}
         </tbody>
       </table>
     </>
   )
 };
+
+const CollapsibleRow = ({ rowInfo, collapsedItem }) => {
+
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  const collapseFunc = () => setIsCollapsed(!isCollapsed);
+  const isRowContainingData = rowInfo.stools.length > 0;
+  return (
+    <>
+      <tr onClick={isRowContainingData ? collapseFunc : () => { }} >
+        <TableBodyCell>{
+          isRowContainingData &&
+          <IconButton onClick={collapseFunc}>
+            {isCollapsed ? <KeyboardArrowRightIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>}
+        </TableBodyCell>
+        <TableBodyCell>{moment(rowInfo.dateString).format('dddd, Do MMMM')}</TableBodyCell>
+        <TableBodyCell center>
+          <StoolCount count={rowInfo.count}>{rowInfo.count}</StoolCount>
+        </TableBodyCell>
+      </tr>
+      <CollapsedRowItem>
+        {!isCollapsed &&
+          <td colSpan={3}>
+            <ListStoolRecords recordedStools={rowInfo.stools} />
+          </td>
+        }
+      </CollapsedRowItem>
+    </>
+  )
+}
+
+const CollapsedRowItem = styled.tr`
+  display: table-row;
+  vertical-align: middle;
+`
+
+
 
 export default SevenDayStoolCountTable;
---
+++ src/components/table/composites/__stories__/SevenDayStoolCountTable.stories.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import moment from 'moment'
+import { BasicButton } from "../../../button"
+import { STOOL_SIZES } from "../../../form/stool/state/stoolModelEnums"
+import { STOOL_DATESTRING_FORMAT } from "../../../form/stool/state/stoolModel"
+
+export default {
+  title: "Tables/Composites/Seven Day Stool Count"
+}
+
+export const Info = () => <p>The following components are demonstrations of table composite components</p>
+
+import SevenDayStoolCountTable from '../SevenDayStoolCountTable'
+export const NoRecords = () => <SevenDayStoolCountTable />
+
+export const MixRecords = () => {
+
+
+
+  const createRandomRecord = () => {
+    const getRandomType = () => Math.ceil(Math.random() * 7);
+    const getRandomDay = () => moment().subtract(getRandomType(), 'days').format();
+    const getRandomSize = () => {
+      switch (
+      Math.ceil(Math.random() * 3)) {
+        case 1: return STOOL_SIZES.SMALL;
+        case 2: return STOOL_SIZES.MEDIUM;
+        case 3: return STOOL_SIZES.LARGE;
+      }
+    }
+    const getRandomDateOnly = () => {
+      if ((Math.random() * 10) > 5) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return {
+      type: getRandomType(),
+      dateTime: { timestamp: getRandomDay(), dateString: moment(getRandomDay()).format(STOOL_DATESTRING_FORMAT), dateOnly: getRandomDateOnly() },
+      size: getRandomSize()
+    }
+  }
+  const createRandomMockStoolRecords = () => [...Array(15)].map((_, i) => createRandomRecord())
+
+  const [mockStoolRecords, setMockStoolRecords] = useState(createRandomMockStoolRecords());
+  return (
+    <>
+      <SevenDayStoolCountTable recordedStools={mockStoolRecords} />
+      <BasicButton onClick={() => setMockStoolRecords(createRandomMockStoolRecords())}>Randomize Stools</BasicButton>
+    </>
+  )
+}
+
+
+
+
