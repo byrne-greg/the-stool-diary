b2c94e15c27c63c1e260f3a113508e2c55e0f40b
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-07-30 14:16:47 +0100

    changing collapsible table to show non-collapse icon on rows without collapsed data

--- src/components/table/CollapsibleTable.js
+++ src/components/table/CollapsibleTable.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components'
 import { makeStyles } from '@material-ui/core/styles';
 import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
@@ -10,6 +11,7 @@ import MaterialTableHead from '@material-ui/core/TableHead';
 import MaterialTableRow from '@material-ui/core/TableRow';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import RemoveIcon from '@material-ui/icons/Remove';
 
 const CollapsibleTable = ({ tableData }) => {
 
@@ -51,12 +53,17 @@ const CollapsibleRow = ({ row }) => {
   const classes = useCollapsibleRowStyles();
   return (
     <>
-      <MaterialTableRow onClick={isCollapsibleDataInRow(row) ? collapse : () => { }} className={isCollapsibleDataInRow(row) ? classes.collapsedRow : null}>
+      <MaterialTableRow
+        onClick={isCollapsibleDataInRow(row) ? collapse : () => { }}
+        className={isCollapsibleDataInRow(row) ? classes.collapsedRow : null}>
         <MaterialTableCell> {/* only show a collapse toggle if there is data to collapse */}
-          {isCollapsibleDataInRow(row) &&
+          {isCollapsibleDataInRow(row) ?
             <IconButton aria-label="expand row" size="small" onClick={collapse}>
               {isCollapsed ? <KeyboardArrowRightIcon /> : <KeyboardArrowDownIcon />}
-            </IconButton>}
+            </IconButton>
+            : <NonButtonMaterialIconHolder>
+              <RemoveIcon />
+            </NonButtonMaterialIconHolder>}
         </MaterialTableCell>
         {row.data.map(item =>
           <MaterialTableCell align={item.align ? item.align : 'left'}>
@@ -64,13 +71,14 @@ const CollapsibleRow = ({ row }) => {
           </MaterialTableCell>
         )}
       </MaterialTableRow>
-      {row.collapsedData &&
-        <CollapsedRow colSpan={row.data.length + 1} collapsedData={row.collapsedData} isShowing={!isCollapsed} />}
+      {row.collapsedData ?
+        <CollapsedRow colSpan={row.data.length + 1} collapsedData={row.collapsedData} isShowing={!isCollapsed} />
+        : null}
     </>
   )
 }
 
-const CollapsedRow = ({ collapsedData, isShowing, colSpan }) => {
+const CollapsedRow = ({ collapsedData = { display: null }, isShowing, colSpan }) => {
 
   const useCollapsedRowStyles = makeStyles({
     collapsedRow: {
@@ -90,3 +98,11 @@ const CollapsedRow = ({ collapsedData, isShowing, colSpan }) => {
     </MaterialTableRow>
   )
 }
+
+// --------
+
+const NonButtonMaterialIconHolder = styled.div`
+  flex: 0 0 auto;
+  padding: 3;
+  color: rgba(0, 0, 0, 0.54);
+`
