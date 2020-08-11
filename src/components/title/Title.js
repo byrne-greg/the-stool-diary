ac04c1138b05b4a49072b2c8e11a4523329a9181
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-08-11 16:12:33 +0100

    add new title component

---
+++ src/components/title/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const validTitleElements = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+const Title = ({as: Tag = 'p', children}) => {
+
+  return validTitleElements.filter(titleElm => titleElm === Tag) ? <Tag>{children}</Tag> : <p>{children}</p>
+}
+
+export default Title
