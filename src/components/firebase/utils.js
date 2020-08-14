ae7d9ba5accc7f04941cbc1547322a78c547f319
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-08-14 16:07:57 +0100

    add firebase methods to login and signup

--- src/components/firebase/utils.js
+++ src/components/firebase/utils.js
@@ -1,5 +1,57 @@
 import firebase from 'gatsby-plugin-firebase'
 
+// ----- AUTH -----
+// https://firebase.google.com/docs/auth/web/start
+
+export const signUpUser = ({email=null, password=null}) => {
+  // TODO
+  if(email && password) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    })
+  }
+}
+
+export const signInUser = ({email=null, password=null}) => {
+  // TODO
+  if(email && password) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    })
+  }
+}
+
+export const isUserSignedIn = () => {
+  // TODO is this the right usage?
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+      // return user;
+      return true;
+    } else {
+      // User is signed out.
+      // ...
+      return false;
+    }
+  });
+}
+
+// ----- DATA -----
+
 export const persistData = (namespace, obj) => {
   firebase.firestore().collection(namespace).add({ ...obj })
     // .then(function (docRef) {
