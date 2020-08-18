4d7f99016f276f7099a44b50459883df3d038897
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-08-18 17:02:50 +0100

    temp information on more work on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,8 @@ import { Link } from "gatsby"
 import { PageLayout } from "../components/layout"
 
 
-const WIP = () => {
+const TestLinks = () => {
   return (
-    <>
-      {/* <ListStoolRecords /> */}
-    </>
-  )
-}
-
-const IndexPage = () => (
-  <PageLayout title="Home">
-    <WIP />
     <ul>
       <li>
         <Link to="/test-signin/">Test Sign-in Page</Link>
@@ -39,9 +30,43 @@ const IndexPage = () => (
       <li>
         <Link to="/test-stool-summary-capture/">Test Form/Stool Capture Summary Screen</Link>
       </li>
-
-
     </ul>
+  )
+}
+
+const IndexPage = () => (
+  <PageLayout title="Home">
+    <TestLinks />
+    <hr/>
+    <div>
+      Header - small logo plus brand with opposite side having a drawer menu/page links
+    </div>
+    <div>
+      Title ("Record with the Stool Diary")
+    </div>
+    <div>
+      Subtitle ("Track your bowel movements for your health")
+    </div>
+    <div>
+      <p>Hero</p>
+      <p>Why do it - 3 cards with icon, title, and blurb why its important</p>
+      <p>Signup/SignIn link buttons</p>
+    </div>
+    <div>
+      <p>Social Review - quotes from users</p>
+    </div>
+    <div>
+      <p>FAQ?</p>
+    </div>
+    <div>
+      <p>Footer</p>
+      <p>Includes a sitemap, provider information, and anything else ancillary</p>
+    </div>
+    <div>
+      <p>See <a href="https://www.impactbnd.com/blog/landing-page-examples">Landing page examples for more information</a></p>
+    </div>
+
+    
   </PageLayout>
 
 )
