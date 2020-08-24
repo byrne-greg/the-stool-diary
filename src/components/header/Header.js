9b0c3ee1e11589276834e11ffe19e48d2371d45f
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-08-24 11:22:32 +0100

    remove styled components from Header comp

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,32 @@
 import React from "react";
-import styled from "styled-components";
 import PropTypes from "prop-types";
+import { makeStyles } from '@material-ui/core/styles';
+import COLORS from '../../utils/colors'
 import { PageCenter } from "../layout";
 import { HeaderLink } from '.';
 
-const HeaderStyle = styled.header`
-  background: rebeccapurple;
-  margin-bottom: 1.45rem;
-`
+const useStyles = makeStyles({
+  header: {
+    background: COLORS.PURPLE,
+    marginBottom: '1.45rem'
+  },
+  title: {
+    margin: 0
+  }
+})
 
-const Header = ({ siteTitle }) => (
-  <HeaderStyle>
-    <PageCenter>
-      <h1 style={{ margin: 0 }}>
-        <HeaderLink>{siteTitle}</HeaderLink>
-      </h1>
-    </PageCenter>
-  </HeaderStyle>
-)
+const Header = ({ siteTitle }) => {
+  const classes = useStyles();
+  return (
+    <header className={classes.header}>
+      <PageCenter>
+        <h1 className={classes.title}>
+          <HeaderLink>{siteTitle}</HeaderLink>
+        </h1>
+      </PageCenter>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -28,3 +37,4 @@ Header.defaultProps = {
 }
 
 export default Header;
+
