8de235037cba513728a0d531c1316f3b018b6bf9
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-08-26 09:49:05 +0100

    removing styled components from headerlink

--- src/components/header/HeaderLink.js
+++ src/components/header/HeaderLink.js
@@ -1,14 +1,21 @@
 import React from "react"
 import { Link as GatsbyLink } from "gatsby"
-import styled from "styled-components"
+import { makeStyles } from '@material-ui/core/styles';
+import ROUTES from "../../utils/routes"
 
-const LinkStyle = styled(GatsbyLink)`
-  color: white;
-  text-decoration: none;
-`
+const useStyles = makeStyles({
+  headerLink: {
+    color: 'white',
+    textDecoration: 'none'
+  }
+})
 
-const HeaderLink = ({ to = "/", children }) => (
-  <LinkStyle to="/">{children}</LinkStyle>
-)
+const HeaderLink = ({ to = ROUTES.HOME, children }) => {
+  const classes = useStyles();
+
+  return (
+    <GatsbyLink className={classes.headerLink} to={to}>{children}</GatsbyLink>  
+  )
+}
 
 export default HeaderLink
