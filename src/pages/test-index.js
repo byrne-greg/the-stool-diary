b526363c3f0e952221056409c52922c32070984d
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-08-26 15:24:34 +0100

    rename test.js page so it doesn't get picked up by npm run test

--- src/pages/test-index.js
+++ src/pages/test-index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 import { PageLayout } from "../components/layout"
 
-
 const TestLinks = () => {
   return (
     <ul>
@@ -34,39 +33,11 @@ const TestLinks = () => {
   )
 }
 
-const TestPage = () => (
+const TestIndexPage = () => (
   <PageLayout title="Test">
     <TestLinks />
-    <hr/>
-    <div>
-      Header - small logo plus brand with opposite side having a drawer menu/page links
-    </div>
-    <div>
-      Title ("Record with the Stool Diary")
-    </div>
-    <div>
-      Subtitle ("Track your bowel movements for your health")
-    </div>
-    <div>
-      <p>Hero</p>
-      <p>Why do it - 3 cards with icon, title, and blurb why its important</p>
-      <p>Signup/SignIn link buttons</p>
-    </div>
-    <div>
-      <p>Social Review - quotes from users</p>
-    </div>
-    <div>
-      <p>FAQ?</p>
-    </div>
-    <div>
-      <p>Footer</p>
-      <p>Includes a sitemap, provider information, and anything else ancillary</p>
-    </div>
-    <div>
-      <p>See <a href="https://www.impactbnd.com/blog/landing-page-examples">Landing page examples for more information</a></p>
-    </div>
   </PageLayout>
 
 )
 
-export default TestPage
+export default TestIndexPage
