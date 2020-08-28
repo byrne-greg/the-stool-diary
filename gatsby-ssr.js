6a562e993f37253bae2677e25ac2857d06377fcf
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-08-28 10:34:39 +0100

    replicate gatsby-browser changes to gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,3 +8,20 @@
 import "firebase/auth"
 import "firebase/firestore"
 import "firebase/functions"
+// import "./src/components/i18n/i18n"
+
+// This API allows wrapping of the root Component in Gatsby
+// Use: Wrapping a global Context for web app state management
+// Use: Wrapping a Theme for the web app
+import React from 'react';
+import GlobalContextProvider from './src/context/GlobalContextProvider'
+import GlobalTheme from './src/components/theme/GlobalTheme'
+
+export const wrapRootElement = ({ element }) => {
+  return (
+  <GlobalContextProvider>
+    <GlobalTheme>
+      {element}
+    </GlobalTheme>
+  </GlobalContextProvider>)
+};
