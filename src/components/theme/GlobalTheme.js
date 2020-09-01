eb7588e700ed178de46c81db1212ccd30985e096
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-09-01 09:35:36 +0100

    adding colors to global theme

--- src/components/theme/GlobalTheme.js
+++ src/components/theme/GlobalTheme.js
@@ -5,6 +5,36 @@ import { CssBaseline } from '@material-ui/core';
 
 let theme = createMuiTheme({
   palette: {
+    primary: { 
+      light: COLORS.THEME.PRIMARY.LIGHT.COL,
+      main: COLORS.THEME.PRIMARY.MAIN.COL,
+      dark: COLORS.THEME.PRIMARY.DARK.COL,
+    },
+    secondary: { 
+      light: COLORS.THEME.SECONDARY.LIGHT.COL,
+      main: COLORS.THEME.SECONDARY.MAIN.COL,
+      dark: COLORS.THEME.SECONDARY.DARK.COL,
+    },
+    warning: { 
+      light: COLORS.THEME.WARNING.LIGHT.COL,
+      main: COLORS.THEME.WARNING.MAIN.COL,
+      dark: COLORS.THEME.WARNING.DARK.COL,
+    },
+    error: { 
+      light: COLORS.THEME.ERROR.LIGHT.COL,
+      main: COLORS.THEME.ERROR.MAIN.COL,
+      dark: COLORS.THEME.ERROR.DARK.COL,
+    },
+    success: { 
+      light: COLORS.THEME.SUCCESS.LIGHT.COL,
+      main: COLORS.THEME.SUCCESS.MAIN.COL,
+      dark: COLORS.THEME.SUCCESS.DARK.COL,
+    },
+    info: { 
+      light: COLORS.THEME.INFO.LIGHT.COL,
+      main: COLORS.THEME.INFO.MAIN.COL,
+      dark: COLORS.THEME.INFO.DARK.COL,
+    },
     background: {
       default: COLORS.WHITE
     }
