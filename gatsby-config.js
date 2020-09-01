05a5d1f0bb203c2ed6703b8cc4e8084541a75491
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-09-01 09:18:37 +0100

    remove typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     inceptionYear: 2020,
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-material-ui`,
       options: {
@@ -20,13 +21,7 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
+    `gatsby-plugin-styled-components`,
     {
       resolve: "gatsby-plugin-firebase",
       options: {
@@ -62,7 +57,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
