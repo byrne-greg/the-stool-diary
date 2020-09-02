20d144735040fc36b9d81089d10e6713f0b22c13
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-09-02 16:51:22 +0100

    fix text button export

--- src/components/button-mui/TextButton.js
+++ src/components/button-mui/TextButton.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 
-export const TextButton = ({ children, ...props }) => (
+const TextButton = ({ children, ...props }) => (
   <Button {...props}>{children}</Button>
 )
+export default TextButton
