db1350576949a489bbf37a9ee4e54c737f734e29
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-09-03 13:24:24 +0100

    removing specifying light and dark colors in theme

--- src/components/theme/GlobalTheme.js
+++ src/components/theme/GlobalTheme.js
@@ -3,48 +3,47 @@ import { createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui
 import COLORS from '../../utils/colors';
 import { CssBaseline } from '@material-ui/core';
 
-let theme = createMuiTheme({
+const baseTheme = createMuiTheme({
   palette: {
     primary: { 
-      light: COLORS.THEME.PRIMARY.LIGHT.COL,
+      // light: COLORS.THEME.SECONDARY.LIGHT.COL,
       main: COLORS.THEME.PRIMARY.MAIN.COL,
-      dark: COLORS.THEME.PRIMARY.DARK.COL,
+      // dark: COLORS.THEME.SECONDARY.DARK.COL,
     },
     secondary: { 
-      light: COLORS.THEME.SECONDARY.LIGHT.COL,
+      // light: COLORS.THEME.SECONDARY.LIGHT.COL,
       main: COLORS.THEME.SECONDARY.MAIN.COL,
-      dark: COLORS.THEME.SECONDARY.DARK.COL,
+      // dark: COLORS.THEME.SECONDARY.DARK.COL,
     },
     warning: { 
-      light: COLORS.THEME.WARNING.LIGHT.COL,
+      // light: COLORS.THEME.WARNING.LIGHT.COL,
       main: COLORS.THEME.WARNING.MAIN.COL,
-      dark: COLORS.THEME.WARNING.DARK.COL,
+      // dark: COLORS.THEME.WARNING.DARK.COL,
     },
     error: { 
-      light: COLORS.THEME.ERROR.LIGHT.COL,
+      // light: COLORS.THEME.ERROR.LIGHT.COL,
       main: COLORS.THEME.ERROR.MAIN.COL,
-      dark: COLORS.THEME.ERROR.DARK.COL,
+      // dark: COLORS.THEME.ERROR.DARK.COL,
     },
     success: { 
-      light: COLORS.THEME.SUCCESS.LIGHT.COL,
+      // light: COLORS.THEME.SUCCESS.LIGHT.COL,
       main: COLORS.THEME.SUCCESS.MAIN.COL,
-      dark: COLORS.THEME.SUCCESS.DARK.COL,
+      // dark: COLORS.THEME.SUCCESS.DARK.COL,
     },
     info: { 
-      light: COLORS.THEME.INFO.LIGHT.COL,
+      // light: COLORS.THEME.INFO.LIGHT.COL,
       main: COLORS.THEME.INFO.MAIN.COL,
-      dark: COLORS.THEME.INFO.DARK.COL,
+      // dark: COLORS.THEME.INFO.DARK.COL,
     },
     background: {
       default: COLORS.WHITE
     }
   }
 });
-theme = responsiveFontSizes(theme);
-
+const responsiveTheme = responsiveFontSizes(baseTheme);
 const GlobalTheme = ({ children }) => {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={responsiveTheme}>
       <CssBaseline/>
       {children}
     </ThemeProvider>
