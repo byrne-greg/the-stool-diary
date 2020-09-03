a8adcda1596f8c51a4c8f3920031b1f9c37c7e9c
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-09-03 13:23:59 +0100

    adding AppWrapper component containing the global components

---
+++ src/components/app-wrapper/AppWrapper.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import GlobalContextProvider from '../../context/GlobalContextProvider'
+import GlobalTheme from '../theme/GlobalTheme'
+
+// This API allows wrapping of the root Component in Gatsby
+// Use: Wrapping a global Context for web app state management
+// Use: Wrapping a global Theme for the web app
+const AppWrapper = ({ children }) => {
+  return (
+    <GlobalContextProvider>
+      <GlobalTheme>
+        {children}
+      </GlobalTheme>
+    </GlobalContextProvider>
+  )
+}
+export default AppWrapper
