6b8a167fe528fdd29fbd19c5aeb3ef2f5909cc29
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-09-04 12:04:20 +0100

    fix stool form index

--- src/components/form/stool/index.js
+++ src/components/form/stool/index.js
@@ -2,5 +2,6 @@ import StoolTypeCapture from "./StoolTypeCapture"
 import StoolDateTimeCapture from "./StoolDateTimeCapture"
 import StoolSizeCapture from "./StoolSizeCapture"
 import StoolCaptureSummary from "./StoolCaptureSummary"
+import RecordStoolForm from "./RecordStoolForm"
 
-export { StoolTypeCapture, StoolDateTimeCapture, StoolSizeCapture, StoolCaptureSummary }
+export { StoolTypeCapture, StoolDateTimeCapture, StoolSizeCapture, StoolCaptureSummary, RecordStoolForm }
