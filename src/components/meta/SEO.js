f8013a572a973849640e630edf5771608813ad5f
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-09-10 09:05:42 +0100

    adding viewport meta and font link to helmet in line with mui gatsby starter

--- src/components/meta/SEO.js
+++ src/components/meta/SEO.js
@@ -68,7 +68,13 @@ const SEO = ({ description, lang, meta, title }) => {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
   )
 }
 
