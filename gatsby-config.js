2bd4d75d8564641353a2fe55a0178cf1457554b4
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-09-10 10:08:23 +0100

    attempt to fix styling issue in prod using gatsby-theme-material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,8 @@ module.exports = {
     inceptionYear: 2020,
   },
   plugins: [
-    `gatsby-plugin-material-ui`,
+    // `gatsby-plugin-material-ui`,
+    `gatsby-theme-material-ui`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: "gatsby-plugin-firebase",
@@ -49,6 +50,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-bundle-stats',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
