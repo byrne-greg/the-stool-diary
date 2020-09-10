b03c84d387d0dabd517a36900c7b49d3da73d29e
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-09-10 16:19:28 +0100

    adding backend to i18n and removing provider from app-wrapper

--- src/components/app-wrapper/AppWrapper.js
+++ src/components/app-wrapper/AppWrapper.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import GlobalContextProvider from '../../context/GlobalContextProvider'
 import GlobalTheme from '../theme/GlobalTheme'
-import { I18nextProvider } from 'react-i18next';
-import i18n from "../i18n/i18n"
 
 // This API allows wrapping of the root Component in Gatsby
 // Use: Wrapping a global Context for web app state management
@@ -13,9 +11,7 @@ const AppWrapper = ({ children }) => {
   return (
       <GlobalContextProvider>
         <GlobalTheme>
-          <I18nextProvider i18n={i18n}>
-            {children}
-          </I18nextProvider>
+          {children}
         </GlobalTheme>
       </GlobalContextProvider>
   )
--- src/components/i18n/i18n.js
+++ src/components/i18n/i18n.js
@@ -1,12 +1,12 @@
 import i18n from "i18next"
-// import Backend from "i18next-xhr-backend"
+import Backend from "i18next-http-backend"
 import LanguageDetector from "i18next-browser-languagedetector"
 import { initReactI18next } from "react-i18next";
 
 i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
   // learn more: https://github.com/i18next/i18next-http-backend
-  // .use(Backend)
+  .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
@@ -23,15 +23,17 @@ i18n
     // defaultNS: "translation",
     // returnObjects: true,
     debug: process.env.NODE_ENV === 'development',
-
+    saveMissing: process.env.NODE_ENV === 'development',
     interpolation: {
       escapeValue: false, // not needed for react!!
     },
-
     react: {
       wait: true,
       useSuspense: false
     },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    }
   })
 
 export default i18n
