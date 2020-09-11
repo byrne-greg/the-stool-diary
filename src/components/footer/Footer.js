4dabe3462a52107f85e85e1aa125ecf2477ef691
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-09-11 10:00:55 +0100

    add attribution for stool images

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -65,6 +65,7 @@ const Attributions = () => {
   return (
     <div className={classes.attributions}>
       <DiaryIconAttribution/>
+      <StoolImagesAttribution/>
     </div>
   )
 }
@@ -72,7 +73,14 @@ const Attributions = () => {
 const DiaryIconAttribution = () => {
   const classes = useStyles();
   return(
-    <div>Diary Icon made by <a href="https://www.flaticon.com/free-icon/diary_1358966?term=diary&page=4&position=21" title="Adib Sulthon" className={classes.linkDecoration}>Adib Sulthon</a> from <a href="https://www.flaticon.com/" title="Flaticon" className={classes.linkDecoration}>www.flaticon.com</a></div>
+    <div>Diary icon made by <a href="https://www.flaticon.com/free-icon/diary_1358966?term=diary&page=4&position=21" title="Adib Sulthon" className={classes.linkDecoration}>Adib Sulthon</a> from <a href="https://www.flaticon.com/" title="Flaticon" className={classes.linkDecoration}>www.flaticon.com</a></div>
+  )
+}
+
+const StoolImagesAttribution = () => {
+  const classes = useStyles();
+  return(
+    <div>Stool images sourced from freely licensed media file created by <a href="https://web.archive.org/web/20110205170819/http://en.wikipedia.org/wiki/File:Bristol_Stool_Chart.png" title="Stool Images from Wikimedia Commons" className={classes.linkDecoration}>Kyle Thompson from Wikimedia Commons</a></div>
   )
 }
 
