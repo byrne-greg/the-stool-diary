125e99e5b8f035efbf5db1b5d1b3fa46714b76fd
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-09-16 09:12:33 +0100

    fix MonthlyStoolCountTable story controls

--- src/components/table/composites/__stories__/MonthlyStoolCountTable.stories.js
+++ src/components/table/composites/__stories__/MonthlyStoolCountTable.stories.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
+import { Container, makeStyles } from '@material-ui/core'
 import { FilledButton } from "../../../button-mui"
+import MonthlyStoolCountTable from '../MonthlyStoolCountTable'
 import * as mockData from "./mock-data"
 
 export default {
@@ -8,30 +10,43 @@ export default {
 
 export const Info = () => <p>The following components are demonstrations of table composite components</p>
 
-import MonthlyStoolCountTable from '../MonthlyStoolCountTable'
+const useStyles = makeStyles({
+  controls: {
+    padding: '1rem',
+    borderBottom: '2px solid black'
+  }
+})
+
 export const NoRecords = () => <MonthlyStoolCountTable />
 export const MixRecordsInDefaultMonth = () => {
+  const classes = useStyles()
   const [mockStoolRecords, setMockStoolRecords] = useState(mockData.createRandomMockStoolRecords({ numOfRecords: 450, range: 365 }));
   return (
     <>
-      <MonthlyStoolCountTable recordedStools={mockStoolRecords} />
-      <FilledButton onClick={() => setMockStoolRecords(mockData.createRandomMockStoolRecords({ numOfRecords: 450, range: 365 }))}>Randomize Stools</FilledButton>
+     <Container className={classes.controls}>
+        <FilledButton onClick={() => setMockStoolRecords(mockData.createRandomMockStoolRecords({ numOfRecords: 450, range: 365 }))}>Randomize Stools</FilledButton> 
+      </Container>
+      <Container >
+        <MonthlyStoolCountTable recordedStools={mockStoolRecords} />
+      </Container>
     </>
   )
 }
 export const MixRecordsInSpecificMonth = () => {
+  const classes = useStyles()
   const defaultMonth = "200201"
   const [mockStoolRecords, setMockStoolRecords] = useState(mockData.createRandomMockStoolRecords({ defaultMonth: defaultMonth, numOfRecords: 450, range: 365 }));
   return (
     <>
-      <MonthlyStoolCountTable recordedStools={mockStoolRecords} month={defaultMonth} />
-      <FilledButton onClick={() => setMockStoolRecords(mockData.createRandomMockStoolRecords({ defaultMonth: defaultMonth, numOfRecords: 450, range: 365 }))}>Randomize Stools</FilledButton>
+      <Container className={classes.controls}>
+        <FilledButton onClick={() => setMockStoolRecords(mockData.createRandomMockStoolRecords({ defaultMonth: defaultMonth, numOfRecords: 450, range: 365 }))}>Randomize Stools</FilledButton>
+      </Container>
+      <Container>
+        <MonthlyStoolCountTable recordedStools={mockStoolRecords} month={defaultMonth} />
+      </Container>
     </>
   )
 }
-
-
-// ---- utility
 
 
 
