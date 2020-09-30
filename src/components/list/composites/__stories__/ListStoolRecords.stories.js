340aaf032d6455ebbdda2d93396c8a7a600092b3
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-09-30 16:30:58 +0100

    added more ListStoolRecord stories

--- src/components/list/composites/__stories__/ListStoolRecords.stories.js
+++ src/components/list/composites/__stories__/ListStoolRecords.stories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ListStoolRecords from '../ListStoolRecords'
 import { STOOL_SIZES } from '../../../../context/stool/model'
 import { getRandomHistoricalMoment } from './mock-data'
+import { Typography } from '@material-ui/core'
 
 export default {
   title: "List/Composites/List Stool Records"
@@ -25,12 +26,36 @@ export const EmptyStoolRecords = () => {
   )
 }
 
+export const EmptyStoolRecordsWithTitle = () => {
+  return (
+    <ListStoolRecords recordedStools={[]} titleComponent={<Typography variant="h4">A Title</Typography>}/>
+  )
+}
+
+export const SingleRecord = () => {
+  const allRecordedStoolData = [
+    { type: 2, dateTime: { timestamp: mocktime1}, size: STOOL_SIZES.SMALL },
+  ]
+  return (
+    <ListStoolRecords recordedStools={allRecordedStoolData} />
+  )
+}
+
+export const ListWithTitle = () => {
+  const allRecordedStoolData = [
+    { type: 2, dateTime: { timestamp: mocktime1}, size: STOOL_SIZES.SMALL },
+  ]
+  return (
+  <ListStoolRecords recordedStools={allRecordedStoolData} titleComponent={<Typography variant="h4">A Title</Typography>}/>
+  )
+}
+
 const mocktime1 = getRandomHistoricalMoment().format();
 const mocktime2 = getRandomHistoricalMoment().format();
 const mocktime3 = getRandomHistoricalMoment().format();
 
 
-export const MultipleStoolRecords = () => {
+export const MultipleStoolRecordsWithDaySeparators = () => {
   const allRecordedStoolData = [
     { type: 1, dateTime: { timestamp: mocktime1}, size: STOOL_SIZES.SMALL },
     { type: 2, dateTime: { timestamp: mocktime2}, size: STOOL_SIZES.MEDIUM },
@@ -47,3 +72,28 @@ export const MultipleStoolRecords = () => {
     <ListStoolRecords recordedStools={allRecordedStoolData} />
   )
 }
+
+export const MultipleStoolRecordsWithoutDaySeparators = () => {
+  const allRecordedStoolData = [
+    { type: 7, dateTime: { timestamp: mocktime1}, size: STOOL_SIZES.SMALL },
+    { type: 1, dateTime: { timestamp: mocktime2}, size: STOOL_SIZES.SMALL },
+    { type: 2, dateTime: { timestamp: mocktime1}, size: STOOL_SIZES.SMALL },
+    { type: 3, dateTime: { timestamp: getRandomHistoricalMoment().format()}, size: STOOL_SIZES.MEDIUM },
+  ]
+  return (
+    <ListStoolRecords recordedStools={allRecordedStoolData} displayDaySeparators={false} />
+  )
+}
+
+export const MultipleStoolRecordsWithoutSort = () => {
+  const allRecordedStoolData = [
+    { type: 1, dateTime: { timestamp: mocktime1}, size: STOOL_SIZES.SMALL },
+    { type: 2, dateTime: { timestamp: mocktime2}, size: STOOL_SIZES.MEDIUM },
+    { type: 3, dateTime: { timestamp: mocktime3}, size: STOOL_SIZES.LARGE },
+    { type: 4, dateTime: { timestamp: mocktime1}, size: STOOL_SIZES.SMALL },
+  ]
+  return (
+    <ListStoolRecords recordedStools={allRecordedStoolData} hasSort={false}/>
+  )
+}
+ 
