625cf06de190a95a3a16255bcb2d06b1f37ba5ca
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2020-11-16 17:09:17 +0000

    adding mock for i18n to stop console error in SignInForm tests

--- src/components/form/auth/signin/__tests__/SignInForm.test.js
+++ src/components/form/auth/signin/__tests__/SignInForm.test.js
@@ -3,6 +3,7 @@ import { render, fireEvent } from "@testing-library/react"
 import SignInForm from "../SignInForm"
 
 jest.mock("../../../../firebase/utils")
+jest.mock("../../../../i18n")
 
 describe("SignInForm", () => {
   describe("UI", () => {
---
+++ src/components/i18n/__mocks__/i18n.js
@@ -0,0 +1,5 @@
+import i18n from "i18next"
+
+const i18n = { t: (s) => s }
+  
+export default i18n
