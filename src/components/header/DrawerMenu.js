ec3a9d49da992194e62ca700ce5348ca7c9a27a2
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-01-13 15:38:31 +0000

    abstract DrawerMenu from Header

---
+++ src/components/header/DrawerMenu.js
@@ -0,0 +1,71 @@
+import React, { useContext, useState } from "react"
+import { Link } from "gatsby"
+import { useTranslation } from "react-i18next"
+import { makeStyles } from "@material-ui/core/styles"
+import Drawer from "@material-ui/core/Drawer"
+import IconButton from "@material-ui/core/IconButton"
+import MenuIcon from "@material-ui/icons/Menu"
+import CloseIcon from "@material-ui/icons/Close"
+import { List, ListItem } from "@material-ui/core"
+import COLORS from "../../utils/colors"
+import ROUTES from "../../utils/routes"
+import { LanguageSelector } from "../i18n"
+import { SignOutForm } from "../form/auth/signout"
+import { GlobalStateContext } from "../../context/global/GlobalContextProvider"
+
+const useStyles = makeStyles({
+  menuButton: {
+    color: COLORS.PURPLE,
+  },
+})
+
+const DrawerMenu = () => {
+  const classes = useStyles()
+
+  const { user } = useContext(GlobalStateContext)
+  const currentUser = user
+
+  const [isDrawerOpen, setDrawerState] = useState(false)
+  const toggleDrawer = () => setDrawerState(!isDrawerOpen)
+
+  const { t } = useTranslation()
+  const menuRoutes = [
+    { text: t("Home"), route: ROUTES.HOME },
+    { text: t("Record a Stool"), route: ROUTES.RECORD_STOOL },
+    { text: t("My Stools"), route: ROUTES.DASHBOARD },
+    { text: t("Sign Up"), route: ROUTES.SIGN_UP },
+  ]
+
+  if (!currentUser) {
+    menuRoutes.push({ text: t("Sign In"), route: ROUTES.SIGN_IN })
+  }
+
+  return (
+    <>
+      <IconButton onClick={toggleDrawer} className={classes.menuButton}>
+        <MenuIcon fontSize="large" />
+      </IconButton>
+      <Drawer anchor="right" open={isDrawerOpen} onClose={toggleDrawer}>
+        <IconButton onClick={toggleDrawer}>
+          <CloseIcon fontSize="large" />
+        </IconButton>
+        <List>
+          {menuRoutes.map(item => (
+            <ListItem key={item.text}>
+              <Link to={item.route}>{item.text}</Link>
+            </ListItem>
+          ))}
+          {currentUser ? (
+            <ListItem key={`signout`}>
+              <SignOutForm />
+            </ListItem>
+          ) : null}
+          <ListItem>
+            <LanguageSelector />
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
+  )
+}
+export default DrawerMenu
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,22 +1,15 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useContext } from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import { useTranslation } from "react-i18next"
-import PropTypes from "prop-types"
 import { makeStyles } from "@material-ui/core/styles"
-import Drawer from "@material-ui/core/Drawer"
-import IconButton from "@material-ui/core/IconButton"
-import MenuIcon from "@material-ui/icons/Menu"
-import CloseIcon from "@material-ui/icons/Close"
-import { List, ListItem } from "@material-ui/core"
+import Typography from "@material-ui/core/Typography"
 import { PageCenter } from "../layout"
 import COLORS from "../../utils/colors"
 import ROUTES from "../../utils/routes"
-import { LanguageSelector } from "../i18n"
 import { StoolDiaryLogo } from "../images"
-import { getCurrentUser } from "../firebase/utils"
-import { SignOutForm } from "../form/auth/signout"
 import { GlobalStateContext } from "../../context/global/GlobalContextProvider"
-import useUserAuthenticated from "../hooks/useUserAuthenticated"
+import { DrawerMenu } from "."
+import { GolfCourseRounded } from "@material-ui/icons"
 
 const useStyles = makeStyles({
   banner: {
@@ -62,11 +55,11 @@ const Header = () => {
       }
     }
   `)
+  // const siteTitle = t(data.site.siteMetadata.title)
+
   const classes = useStyles()
   const { t } = useTranslation()
   const { user } = useContext(GlobalStateContext)
-
-  const siteTitle = t(data.site.siteMetadata.title)
 
   return (
     <header className={classes.banner}>
@@ -79,7 +72,11 @@ const Header = () => {
               </div>
             </div>
           </Link>
-          {user ? <p>{`Welcome ${user.email}`}</p> : null}
+          {user ? (
+            <Typography variant="h6" component="p">
+              {`${t(`Welcome`)}, ${user.email}`}
+            </Typography>
+          ) : null}
           <div>
             <DrawerMenu />
           </div>
@@ -88,66 +85,4 @@ const Header = () => {
     </header>
   )
 }
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
 export default Header
-
-// -----
-
-const DrawerMenu = () => {
-  const classes = useStyles()
-
-  const { user } = useContext(GlobalStateContext)
-  // const currentUser = useUserAuthenticated()
-  const currentUser = user
-
-  const [isDrawerOpen, setDrawerState] = useState(false)
-  const toggleDrawer = () => setDrawerState(!isDrawerOpen)
-
-  const { t } = useTranslation()
-  const menuRoutes = [
-    { text: t("Home"), route: ROUTES.HOME },
-    { text: t("Record a Stool"), route: ROUTES.RECORD_STOOL },
-    { text: t("My Stools"), route: ROUTES.DASHBOARD },
-    { text: t("Sign Up"), route: ROUTES.SIGN_UP },
-  ]
-
-  if (!currentUser) {
-    menuRoutes.push({ text: t("Sign In"), route: ROUTES.SIGN_IN })
-  }
-
-  return (
-    <>
-      <IconButton onClick={toggleDrawer} className={classes.menuButton}>
-        <MenuIcon fontSize="large" />
-      </IconButton>
-      <Drawer anchor="right" open={isDrawerOpen} onClose={toggleDrawer}>
-        <IconButton onClick={toggleDrawer}>
-          <CloseIcon fontSize="large" />
-        </IconButton>
-        <List>
-          {menuRoutes.map(item => (
-            <ListItem key={item.text}>
-              <Link to={item.route}>{item.text}</Link>
-            </ListItem>
-          ))}
-          {currentUser ? (
-            <ListItem key={`signout`}>
-              <SignOutForm />
-            </ListItem>
-          ) : null}
-          <ListItem>
-            <LanguageSelector />
-          </ListItem>
-        </List>
-      </Drawer>
-    </>
-  )
-}
