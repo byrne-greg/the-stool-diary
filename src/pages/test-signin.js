b1cd14066624b8674233d9a94e1ef727a37751cd
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-01-21 11:55:28 +0000

    add anonymous only user protection to sign-in/sign-up pages

--- src/pages/test-signin.js
+++ src/pages/test-signin.js
@@ -2,17 +2,25 @@ import React from "react"
 import { Link } from "gatsby"
 import { PageLayout } from "../components/layout"
 import SigninScreen from "../components/screens/SigninScreen"
+import { useAnonymousUserOnlyRoute } from "../components/hooks/route-hooks"
 
-const SigninPage = () => (
-  <PageLayout title="Sign In">
-    <SigninScreen />
-    <div></div>
-    <ul>
-      <li>
-        <Link to="/">Go home</Link>
-      </li>
-    </ul>
-  </PageLayout>
-)
+const SigninPage = () => {
+  const isAnonymousUser = useAnonymousUserOnlyRoute()
+  return (
+    <>
+      {isAnonymousUser ? (
+        <PageLayout title="Sign In">
+          <SigninScreen />
+          <div></div>
+          <ul>
+            <li>
+              <Link to="/">Go home</Link>
+            </li>
+          </ul>
+        </PageLayout>
+      ) : null}
+    </>
+  )
+}
 
 export default SigninPage
--- src/pages/test-signup.js
+++ src/pages/test-signup.js
@@ -2,17 +2,25 @@ import React from "react"
 import { Link } from "gatsby"
 import { PageLayout } from "../components/layout"
 import SignupScreen from "../components/screens/SignupScreen"
+import { useAnonymousUserOnlyRoute } from "../components/hooks/route-hooks"
 
-const SignupPage = () => (
-  <PageLayout title="Sign-Up">
-    <SignupScreen />
-    <div></div>
-    <ul>
-      <li>
-        <Link to="/">Go home</Link>
-      </li>
-    </ul>
-  </PageLayout>
-)
+const SignupPage = () => {
+  const isAnonymousUser = useAnonymousUserOnlyRoute()
+  return (
+    <>
+      {isAnonymousUser ? (
+        <PageLayout title="Sign Up">
+          <SignupScreen />
+          <div></div>
+          <ul>
+            <li>
+              <Link to="/">Go home</Link>
+            </li>
+          </ul>
+        </PageLayout>
+      ) : null}
+    </>
+  )
+}
 
 export default SignupPage
