e9c067581e46a3071ed04ae0157ad1f9a39eebf8
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-01-22 09:30:05 +0000

    fix build issue with gatsby and firebase

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,3 +1,8 @@
 import firebase from "gatsby-plugin-firebase"
 
-export const firebaseAuth = firebase.auth()
+// Included because Firebase expects the window object to be included but when run on Gatsby build, it doesn't, causing build errors
+// https://github.com/gatsbyjs/gatsby/issues/16181
+const isBrowser = () => typeof window !== "undefined"
+const Firebase = isBrowser() ? firebase : { auth: () => {} }
+
+export const firebaseAuth = Firebase.auth()
