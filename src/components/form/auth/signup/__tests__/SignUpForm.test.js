49a5687f64d833ada000057e4c91ea25a79547e4
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-01-29 15:42:49 +0000

    add UI render tests for SignUpForm

--- src/components/form/auth/signup/__tests__/SignUpForm.test.js
+++ src/components/form/auth/signup/__tests__/SignUpForm.test.js
@@ -27,5 +27,85 @@ describe("SignInForm", () => {
       // ASSERT
       expect(container).toBeDefined()
     })
+    test(`when rendered, then a forename field should be visible`, async () => {
+      // ARRANGE
+
+      // ACT
+      const { queryByTestId } = render(<SignUpForm />)
+      const forenameInput = queryByTestId(
+        "sign-up-forename-input"
+      ).querySelector("input")
+
+      // ASSERT
+      expect(forenameInput).toBeTruthy()
+    })
+    test(`when rendered, then a surname field should be visible`, async () => {
+      // ARRANGE
+
+      // ACT
+      const { queryByTestId } = render(<SignUpForm />)
+      const surnameInput = queryByTestId("sign-up-surname-input").querySelector(
+        "input"
+      )
+
+      // ASSERT
+      expect(surnameInput).toBeTruthy()
+    })
+    test(`when rendered, then an email address field should be visible`, async () => {
+      // ARRANGE
+
+      // ACT
+      const { queryByTestId } = render(<SignUpForm />)
+      const emailAddressInput = queryByTestId(
+        "sign-up-email-input"
+      ).querySelector("input")
+
+      // ASSERT
+      expect(emailAddressInput).toBeTruthy()
+    })
+    test(`when rendered, then a password field should be visible`, async () => {
+      // ARRANGE
+
+      // ACT
+      const { queryByTestId } = render(<SignUpForm />)
+      const passwordInput = queryByTestId(
+        "sign-up-password-input"
+      ).querySelector("input")
+
+      // ASSERT
+      expect(passwordInput).toBeTruthy()
+    })
+    test(`when rendered, then a terms and conditions checkbox should be visible`, async () => {
+      // ARRANGE
+
+      // ACT
+      const { queryByTestId } = render(<SignUpForm />)
+      const termsAndConditionsCheck = queryByTestId(
+        "sign-up-tc-check"
+      ).querySelector("input")
+
+      // ASSERT
+      expect(termsAndConditionsCheck).toBeTruthy()
+    })
+    test(`when rendered, then a terms and conditions link should be visible`, async () => {
+      // ARRANGE
+
+      // ACT
+      const { queryByTestId } = render(<SignUpForm />)
+      const termsAndConditionsLink = queryByTestId("sign-up-tc-link")
+
+      // ASSERT
+      expect(termsAndConditionsLink).toBeTruthy()
+    })
+    test(`when rendered, then the sign-in link should be visible`, async () => {
+      // ARRANGE
+
+      // ACT
+      const { queryByTestId } = render(<SignUpForm />)
+      const signInLink = queryByTestId("sign-up-sign-in-link")
+
+      // ASSERT
+      expect(signInLink).toBeTruthy()
+    })
   })
 })
