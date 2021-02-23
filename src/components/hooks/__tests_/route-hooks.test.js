6165bd1c35050cf654e41e1f4b1fc6d6f928ff7b
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-02-23 22:32:26 +0000

    add anon user hook route test

--- src/components/hooks/__tests_/route-hooks.test.js
+++ src/components/hooks/__tests_/route-hooks.test.js
@@ -69,7 +69,7 @@ describe("Auth User Only Redirect Hook", () => {
     expect(gatsby.navigate).toHaveBeenCalledWith(ROUTES.SIGN_IN)
   })
 
-  test("when authenticated and user navigates to component with auth user only access, then user is redirected to sign in page", () => {
+  test("when authenticated and user navigates to component with auth user only access, then user is not redirected", () => {
     // ARRANGE
     gatsby.navigate = jest.fn()
 
@@ -83,5 +83,77 @@ describe("Auth User Only Redirect Hook", () => {
     // the first render will call navigate, but the second render won't as it will be updated with an authUser
     expect(hookSpy.mock.calls.length).toBe(2)
     expect(gatsby.navigate).toHaveBeenCalledTimes(1)
+    expect(gatsby.navigate).toHaveBeenCalledWith(ROUTES.SIGN_IN)
+  })
+})
+
+// -----
+
+const AnonUserHookTestComponent = ({ authUser = null }) => {
+  return (
+    <GlobalContextProvider>
+      <AnonUserHookTestController authUser={authUser} />
+    </GlobalContextProvider>
+  )
+}
+
+const AnonUserHookTestController = ({ authUser }) => {
+  const globalDispatch = useContext(GlobalDispatchContext)
+  useEffect(() => {
+    // eslint-disable-next-line require-jsdoc
+    async function update() {
+      await updateAuthUser(globalDispatch, authUser)
+    }
+    // only update if it's not null user
+    if (authUser) {
+      update()
+    }
+  })
+
+  return <AnonUserHookUnderTest />
+}
+
+const AnonUserHookUnderTest = () => {
+  routeHook.useAnonymousUserOnlyRoute()
+  return <div />
+}
+
+describe("Anon User Only Redirect Hook", () => {
+  let hookSpy
+  beforeEach(() => {
+    hookSpy = jest.spyOn(routeHook, "useAnonymousUserOnlyRoute")
+  })
+  afterEach(() => {
+    hookSpy.mockRestore()
+  })
+
+  test("when unauthenticated and user navigates to component with anon user only access, then user is not redirected", () => {
+    // ARRANGE
+    gatsby.navigate = jest.fn()
+
+    // ACT
+    // single render - no update to the component
+    render(<AnonUserHookTestComponent />)
+
+    // ASSERT
+    expect(hookSpy.mock.calls.length).toBe(1)
+    expect(gatsby.navigate).not.toHaveBeenCalled()
+  })
+
+  test("when authenticated and user navigates to component with anon user only access, then user is redirected to home page", () => {
+    // ARRANGE
+    gatsby.navigate = jest.fn()
+
+    // ACT
+    // double render - one as default then another to update the internal context object
+    act(() => {
+      render(<AnonUserHookTestComponent authUser={{ dummyObj: true }} />)
+    })
+
+    // ASSERT
+    // the first render will call navigate, but the second render won't as it will be updated with an authUser
+    expect(hookSpy.mock.calls.length).toBe(2)
+    expect(gatsby.navigate).toHaveBeenCalledTimes(1)
+    expect(gatsby.navigate).toHaveBeenCalledWith(ROUTES.HOME)
   })
 })
