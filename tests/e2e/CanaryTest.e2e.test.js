8f576d4e46a1474bd5fbe31dfb3073f77a5289e3
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-03-25 09:05:50 +0000

    change HomePage e2e to CanaryTest

--- tests/e2e/CanaryTest.e2e.test.js
+++ tests/e2e/CanaryTest.e2e.test.js
@@ -1,8 +1,8 @@
 import HomePage from "./pageobjects/HomePage.pageobject"
 
-fixture(`Home Page Visual Test`)
+fixture(`Canary Test`)
 
-test("loads and contains all required visual elements", async t => {
+test("home page loads", async t => {
   const homePage = new HomePage(t)
   await homePage.gotoUrl()
   await homePage.verifyTitleExists()
