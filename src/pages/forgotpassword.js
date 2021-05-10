ef3a939583629934c6aba293406cb0b79703c2c3
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-05-10 17:10:49 +0100

    remove home link from ForgotPassword page

--- src/pages/forgotpassword.js
+++ src/pages/forgotpassword.js
@@ -1,17 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 import { PageLayout } from "../components/layout"
 import ForgotPasswordScreen from "../components/screens/ForgotPasswordScreen"
 
 const ForgotPasswordPage = () => (
   <PageLayout title="Forgot Password">
     <ForgotPasswordScreen />
-    <div></div>
-    <ul>
-      <li>
-        <Link to="/">Go home</Link>
-      </li>
-    </ul>
   </PageLayout>
 )
 
