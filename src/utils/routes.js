82dc2b15d1c581f080970610eacaaa7886084bfd
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-05-10 20:18:38 +0100

    change signin route to production

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,6 +1,6 @@
 const HOME = { HOME: "/" }
 const AUTH = {
-  SIGN_IN: "/test-signin",
+  SIGN_IN: "/signin",
   SIGN_UP: "/signup",
   FORGOT_PASSWORD: "/forgotpassword",
   PRIVACY_POLICY: "/test-privacypolicy",
