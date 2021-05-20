684660fef23e6a8dfe1275090d68304fc597803c
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-05-20 08:15:12 +0100

    fix issue with div descendent of p

--- src/components/footer/Attributions.js
+++ src/components/footer/Attributions.js
@@ -15,20 +15,22 @@ const LegoImageAttribution = () => {
   const classes = useStyles()
   return (
     <div>
-      Hero image by{" "}
-      <a
-        href="https://unsplash.com/@danielkcheung?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-        className={classes.linkDecoration}
-      >
-        Daniel Cheung
-      </a>{" "}
-      on{" "}
-      <a
-        href="https://unsplash.com/s/photos/toilet?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-        className={classes.linkDecoration}
-      >
-        Unsplash
-      </a>
+      <Typography className={classes.attributions}>
+        Hero image by{" "}
+        <a
+          href="https://unsplash.com/@danielkcheung?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          className={classes.linkDecoration}
+        >
+          Daniel Cheung
+        </a>{" "}
+        on{" "}
+        <a
+          href="https://unsplash.com/s/photos/toilet?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          className={classes.linkDecoration}
+        >
+          Unsplash
+        </a>
+      </Typography>
     </div>
   )
 }
@@ -37,22 +39,24 @@ const DiaryIconAttribution = () => {
   const classes = useStyles()
   return (
     <div>
-      Diary icon made by{" "}
-      <a
-        href="https://www.flaticon.com/free-icon/diary_1358966?term=diary&page=4&position=21"
-        title="Adib Sulthon"
-        className={classes.linkDecoration}
-      >
-        Adib Sulthon
-      </a>{" "}
-      from{" "}
-      <a
-        href="https://www.flaticon.com/"
-        title="Flaticon"
-        className={classes.linkDecoration}
-      >
-        www.flaticon.com
-      </a>
+      <Typography className={classes.attributions}>
+        Diary icon made by{" "}
+        <a
+          href="https://www.flaticon.com/free-icon/diary_1358966?term=diary&page=4&position=21"
+          title="Adib Sulthon"
+          className={classes.linkDecoration}
+        >
+          Adib Sulthon
+        </a>{" "}
+        from{" "}
+        <a
+          href="https://www.flaticon.com/"
+          title="Flaticon"
+          className={classes.linkDecoration}
+        >
+          www.flaticon.com
+        </a>
+      </Typography>
     </div>
   )
 }
@@ -61,27 +65,26 @@ const StoolImagesAttribution = () => {
   const classes = useStyles()
   return (
     <div>
-      Stool images sourced from freely licensed media file created by{" "}
-      <a
-        href="https://web.archive.org/web/20110205170819/http://en.wikipedia.org/wiki/File:Bristol_Stool_Chart.png"
-        title="Stool Images from Wikimedia Commons"
-        className={classes.linkDecoration}
-      >
-        Kyle Thompson from Wikimedia Commons
-      </a>
+      <Typography className={classes.attributions}>
+        Stool images sourced from freely licensed media file created by{" "}
+        <a
+          href="https://web.archive.org/web/20110205170819/http://en.wikipedia.org/wiki/File:Bristol_Stool_Chart.png"
+          title="Stool Images from Wikimedia Commons"
+          className={classes.linkDecoration}
+        >
+          Kyle Thompson from Wikimedia Commons
+        </a>
+      </Typography>
     </div>
   )
 }
 
 const Attributions = () => {
-  const classes = useStyles()
   return (
     <div>
-      <Typography className={classes.attributions}>
-        <DiaryIconAttribution />
-        <StoolImagesAttribution />
-        <LegoImageAttribution />
-      </Typography>
+      <DiaryIconAttribution />
+      <StoolImagesAttribution />
+      <LegoImageAttribution />
     </div>
   )
 }
