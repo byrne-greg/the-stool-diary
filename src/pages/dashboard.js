d95bf7977b215a1fe8a6e353d96791c0a6089cee
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-05-26 10:32:36 +0100

    remove console log statements from dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,13 +1,10 @@
-// Gatsby supports TypeScript natively!
 import React from "react"
-import { Link } from "gatsby"
 import { PageLayout } from "../components/layout"
 import { useAuthUserOnlyRoute } from "../components/hooks/route-hooks"
 import { DashboardScreen } from "../components/screens"
 
 const DashboardPage = () => {
   const isAuthenticated = useAuthUserOnlyRoute()
-  console.log("Dashboard.isAuthenticated", isAuthenticated)
 
   return (
     <>
