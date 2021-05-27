be00d5999ab8e1e08f4a492a9817c34141cb126d
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-05-27 17:20:28 +0100

    complete the record stool submit page actions

--- src/components/screens/RecordStoolFormScreen.js
+++ src/components/screens/RecordStoolFormScreen.js
@@ -1,13 +1,20 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
 import { useTranslation } from "react-i18next"
-import PropTypes from "prop-types"
 import Typography from "@material-ui/core/Typography"
 import { RecordStoolForm } from "../form/stool"
 import { persistStoolData } from "../../context/stool/persistence"
+import { GlobalStateContext } from "../../context/global/GlobalContextProvider"
+import {
+  SeeStoolDiaryActionCard,
+  SignUpActionCard,
+  LogInActionCard,
+} from "../card-mui/composite"
+import { CardContainer } from "../card-mui"
 
 const RecordStoolFormScreen = () => {
   const [isUserFinished, setIsUserFinished] = useState(false)
   const { t } = useTranslation()
+  const { authUser } = useContext(GlobalStateContext)
   return (
     <>
       {!isUserFinished ? (
@@ -29,10 +36,30 @@ const RecordStoolFormScreen = () => {
             {t("Submitted")}
           </Typography>
           <Typography gutterBottom variant="body1" component="p">
-            {t(
-              "Fantastic! You've just finished recording a stool. See your stool record on the My Stools page."
-            )}
+            {t("Fantastic! You've just finished recording a stool.")}
           </Typography>
+          {authUser ? (
+            <>
+              <Typography>
+                {t("Why not check out your stool record right now?")}
+              </Typography>
+              <CardContainer cardWidth={"300px"}>
+                <SeeStoolDiaryActionCard />
+              </CardContainer>
+            </>
+          ) : (
+            <>
+              <Typography>
+                {t(
+                  "If you want to start saving your stools, why don't you sign-up or sign-in?"
+                )}
+              </Typography>
+              <CardContainer cardWidth={"300px"}>
+                <SignUpActionCard />
+                <LogInActionCard />
+              </CardContainer>
+            </>
+          )}
         </div>
       )}
     </>
